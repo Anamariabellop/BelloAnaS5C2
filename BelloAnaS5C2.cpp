01ded89ee82bd1a8481e7ae4eb831311ef7b4a6f
Ana Bello <anamariabellopena@MacBook-Air-de-Ana-4.local>
2019-07-04 11:09:03 -0500

    cpp empezado

---
+++ BelloAnaS5C2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<math.h>
+#include<fstream>
+
+using namespace std;
+
+void derivada(double funcion);
+
+int main(){
+
+
+	return 0;
+}
+
+void derivada(double funcion)
+{
+	return -funcion;	
+}
+
+void 
