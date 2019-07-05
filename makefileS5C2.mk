58f058e4fbfa5c6175b66bdb6efbedc1ec0ba2a9
Ana Bello <anamariabellopena@MacBook-Air-de-Ana-4.local>
2019-07-04 22:12:43 -0500

    makefile completo

--- makefileS5C2.mk
+++ makefileS5C2.mk
@@ -4,3 +4,9 @@ datos.dat: a.out
 	./a.out
 a.out: BelloAnaS5C2.cpp
 	g++ BelloAnaS5C2.cpp
+plotS5C2r4.png: datosr4.dat plotS5C2.py BelloAnaS5C2.cpp
+	python plotS5C2.py
+datos.dat: a.out
+	./a.out
+a.out: BelloAnaS5C2.cpp
+	g++ BelloAnaS5C2.cpp
