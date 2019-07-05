aee64676c316fb832b03b4567cb7350a93031d3a
Ana Bello <anamariabellopena@MacBook-Air-de-Ana-4.local>
2019-07-04 22:13:20 -0500

    plot con Rk4

--- plotS5C2.py
+++ plotS5C2.py
@@ -1,3 +1,29 @@
 import numpy as np 
 import matplotlib.pylab as plt
 
+datos=np.genfromtxt("datos.dat")
+x=datos[:,0]
+y=datos[:,1]
+
+plt.figure()
+plt.plot(x,y, label='y(x)')
+plt.xlabel("x")
+plt.ylabel("y")
+plt.ylim(-0.2,2)
+plt.title("Euler")
+plt.legend()
+plt.savefig("plotS5C2.png")
+
+
+datosrunge=np.genfromtxt("datosr4.dat")
+xr=datos[:,0]
+yr=datos[:,1]
+
+plt.figure()
+plt.plot(x,y, color='m',label='y(x)')
+plt.xlabel("x")
+plt.ylabel("y")
+plt.ylim(-0.2,2)
+plt.legend()
+plt.title("Runge-Kutta")
+plt.savefig("plotS5C2r4.png")
