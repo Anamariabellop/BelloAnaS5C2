a36d2ecae5ea1728f98bcc58cf80c9591a2955b6
Ana Bello <anamariabellopena@MacBook-Air-de-Ana-4.local>
2019-07-04 22:22:38 -0500

    make corregido

--- makefileS5C2.mk
+++ makefileS5C2.mk
@@ -1,3 +1,4 @@
+#makefile
 plotS5C2.png: datos.dat plotS5C2.py BelloAnaS5C2.cpp
 	python plotS5C2.py
 datos.dat: a.out
@@ -6,7 +7,7 @@ a.out: BelloAnaS5C2.cpp
 	g++ BelloAnaS5C2.cpp
 plotS5C2r4.png: datosr4.dat plotS5C2.py BelloAnaS5C2.cpp
 	python plotS5C2.py
-datos.dat: a.out
+datosr4.dat: a.out
 	./a.out
 a.out: BelloAnaS5C2.cpp
 	g++ BelloAnaS5C2.cpp
